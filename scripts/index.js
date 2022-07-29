8522cc599e4fdb99431a9e2bde0825be84521b9a
MarinaN89 <MarinaNep89@yandex.ru>
2022-07-29 15:41:25 +0300

    исправлены ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,27 @@
-const formEle = document.querySelector(".popup");
-const title = document.querySelector(".profile__title");
-const subTitle = document.querySelector(".profile__subtitle");
-const showBtn = document.querySelector(".button-open");
-const nameInput = formEle.querySelector("#form-heading");
-const subTitleInput = formEle.querySelector("#form-subtitle");
-const changeBtn = formEle.querySelector(".form__container-button_submit");
-const popupClose = document.querySelector(".popup__close");
+const popupElement = document.querySelector('.popup');
+const title = document.querySelector('.profile__info-title');
+const subTitle = document.querySelector('.profile__info-subtitle');
+const openButton = document.querySelector('.profile__edit-button');
+const profileNameInput = popupElement.querySelector('#form-heading');
+const profileSubTitleInput = popupElement.querySelector('#form-subtitle');
+const saveButton = popupElement.querySelector('.form__container-button');
+const popupClose = document.querySelector('.popup__close');
 
 
-showBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  nameInput.value = title.textContent;
-  subTitleInput.value = subTitle.textContent;
-  formEle.classList.toggle("popup_opened");
+openButton.addEventListener('click', (e) => {
+  profileNameInput.value = title.textContent;
+  profileSubTitleInput.value = subTitle.textContent;
+  popupElement.classList.toggle('popup_opened');
 });
 
-changeBtn.addEventListener("click", (e) => {
+saveButton.addEventListener('click', (e) => {
   e.preventDefault();
-  title.textContent = nameInput.value;
-  subTitle.textContent = subTitleInput.value;
-  formEle.classList.toggle("popup_opened");
-  popupElement.classList.remove("popup_opened");
+  title.textContent = profileNameInput.value;
+  subTitle.textContent = profileSubTitleInput.value;
+  popupElement.classList.remove('popup_opened');
 });
 
 function closePopup(){
-    formEle.classList.remove("popup_opened");
+  popupElement.classList.remove('popup_opened');
 }
-popupClose.addEventListener("click", closePopup);
+popupClose.addEventListener('click', closePopup);
